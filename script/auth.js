329fcad95c0f1ea2cfa8f9cc1567c30274579634
Ali Abid <aliabid94@gmail.com>
2019-10-14 20:41:09 -0700

    test in or out

--- script/auth.js
+++ script/auth.js
@@ -1,18 +1,21 @@
-function onLoadCallback() {
-  auth2 = gapi.auth2.getAuthInstance();
+// function onLoadCallback() {
+//   gapi.auth2.init({
+//     client_id: '64779547743-pr0sphmso99ua16otnsj7kvejlqlg7eb.apps.googleusercontent.com'
+//   });
 
-  if (auth2.isSignedIn.get()) {
-    var profile = auth2.currentUser.get().getBasicProfile();
-    $("#fullname").text(profile.getName())
-    $(".in").show();
-  } else {
-    $(".out").show();  
-  }
-}
+//   // auth2 = gapi.auth2.getAuthInstance();
+//   // if (auth2.isSignedIn.get()) {
+//     // var profile = auth2.currentUser.get().getBasicProfile();
+//     // $("#fullname").text(profile.getName())
+//     // $(".in").show();
+//   // } else {
+//     // $(".out").show();  
+//   // }
+// }
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    location.reload(); 
-  });
-}
+// function signOut() {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//     location.reload(); 
+//   });
+// }
