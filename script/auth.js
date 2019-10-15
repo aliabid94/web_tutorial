72c764dc90f52d196d63f8ee65c90ee6aad6fc4c
Ali Abid <aliabid94@gmail.com>
2019-10-14 20:30:34 -0700

    test in or out

--- script/auth.js
+++ script/auth.js
@@ -2,8 +2,17 @@ function onLoadCallback() {
   auth2 = gapi.auth2.getAuthInstance();
 
   if (auth2.isSignedIn.get()) {
+    var profile = auth2.currentUser.get().getBasicProfile();
+    $("#fullname").text(profile.getName())
     $(".in").show();
   } else {
     $(".out").show();  
   }
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    location.reload(); 
+  });
 }
